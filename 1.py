7194ccd05b6bdc06f789f1745496ae886bbc3dcd
홍주이 <hong200003@naver.com>
2023-04-05 16:33:19 +0900

    break cotinue

--- 1.py
+++ 1.py
@@ -115,5 +115,64 @@ for i in range(1,11,1) :
     #sum = sum + i
     print(i,"번째 loop입니다. sum은 " ,sum," 입니다")
     
-    '''
+  
 
+#sum , 0-10까지 숫자를 찍음
+#sum 을 구할것임
+sum = 0
+n = 0
+while n < 11 :
+    #print("n : " ,n)
+    sum += n
+    print(n,"  번째 sum : " , sum )
+    #print(n,end=" ")
+    n = n+1
+
+print("while밖에서 sum 의 값 : " , sum)
+
+while 0 :
+    print("실행이 되지않음")
+
+print("while 0 다음줄입니다")
+
+
+#while True :
+ #   print("무한루프")
+
+#while False :
+ #   print("실행이 되지않음")
+ 
+   '''
+
+#break continue
+#단어 입력을 무한 루프로 받는다
+#그 글자를 3 번 써줌
+#"exit" -> 루프를 끝내고 종료
+#"apple" -> 3번 안쓰고 그냥 다시 단어를 입력받음
+
+while True : 
+    word = input("단어를 입력하세요 -> ")
+    if word == "exit" :
+         print("넌 exit 를 입력했다. break를 만난다.")
+         break
+         print("break 뒤의 문장")
+    elif word == "apple" :
+         print("넌 apple을 입력했다. continue를 만난다.")
+         continue
+         print("continue 뒤의 문장")
+    else :
+        for i in range(3) :
+            print(word, end=' ')
+        print("해당 단어 끝")
+
+    '''    
+while True :
+    if exit인 경우 :
+        break
+    elif apple인 경우 :
+        continue
+    else :
+        for :
+            3번 찍는다
+
+'''
